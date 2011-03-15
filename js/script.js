4ba7148b174d1c55dc2d35495bc6afce665278fb
davetayls <dave@the-taylors.org>
2011-03-15 13:51:11 +0000

    added map points from json

--- js/script.js
+++ js/script.js
@@ -4,10 +4,20 @@
 (function($){
 	
 	$(function(){
-		$('#map').maps();
+		$('#map').maps({
+			centerPin: false
+		});
 		$.getJSON('AndyExample.json', function(data, status){
-			$(data.locations).each(function(){
-				console.log(this);
+			$(data.locations).each(function(i){
+				console.log(this.lat + ' ' + this.lon);
+				if (i === 0){
+					$.maps.setCenter('map', this.lat, this.lon);
+					$.maps.placePin('map', this.lat, this.lon, '');
+				}else if (this.isKeyPoint){
+					$.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTlarge');					
+				}else{
+					$.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTsmall');					
+				}
 			});
 		});
 	});
