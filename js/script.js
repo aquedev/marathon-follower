d92f5e2da24adb046decc431d5959200f1ecab42
davetayls <dave@the-taylors.org>
2011-03-21 09:06:32 +0000

    tracking pins already added to the map

--- js/script.js
+++ js/script.js
@@ -3,21 +3,29 @@
 */
 (function($){
 	
+	var mapDetails,
+		newPins = {};
+	
 	$(function(){
 		$('#map').maps({
 			centerPin: false
 		});
+		
+		mapDetails = $.maps.getMapDetails('map');
+		
 		$.getJSON('AndyExample.json', function(data, status){
 			$(data.locations).each(function(i){
+				var newPin;
 				console.log(this.lat + ' ' + this.lon);
 				if (i === 0){
 					$.maps.setCenter('map', this.lat, this.lon);
-					$.maps.placePin('map', this.lat, this.lon, '');
+					newPin = $.maps.placePin('map', this.lat, this.lon, '');
 				}else if (this.isKeyPoint){
-					$.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTlarge');					
+					newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTlarge');					
 				}else{
-					$.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTsmall');					
+					newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTsmall');					
 				}
+				newPins[this.id] = newPin;
 			});
 		});
 	});
