0c7d38365eedd08720e861cff99c30dbd98d0893
Dave Taylor <dave@the-taylors.org>
2011-03-22 14:36:58 +0000

    added polling to map pins script

--- js/script.js
+++ js/script.js
@@ -4,30 +4,47 @@
 (function($){
 	
 	var mapDetails,
-		newPins = {};
+		newPins = {},
+        counter = 0,
+        feedUrl = 'AndyExample.json';
 	
+    var sortByCreated = function(a,b){
+        return a.created < b.created ? 1 : a.created > b.created ? -1 : 0;
+    };
+    var updatePins = function(){
+		$.getJSON(feedUrl, function(data, status){
+            if (status === 'success'){
+                var locations = data.locations.sort(sortByCreated);
+                console.log({l:locations});
+                $(locations).each(function(i){
+                    var newPin = newPins[this.id];
+                    if (!newPin){
+                        console.log(this.lat + ' ' + this.lon);
+                        if (i === 0){
+                            $.maps.setCenter('map', this.lat, this.lon);
+                            newPin = $.maps.placePin('map', this.lat, this.lon, '');
+                        }else if (this.isKeyPoint){
+                            newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTlarge');					
+                        }else{
+                            newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTsmall');					
+                        }
+                        newPins[this.id] = newPin;
+                    }
+                });
+                setTimeout(updatePins,3000);
+            }
+		});
+        counter ++;
+    };
+    
 	$(function(){
 		$('#map').maps({
 			centerPin: false
 		});
 		
 		mapDetails = $.maps.getMapDetails('map');
-		
-		$.getJSON('AndyExample.json', function(data, status){
-			$(data.locations).each(function(i){
-				var newPin;
-				console.log(this.lat + ' ' + this.lon);
-				if (i === 0){
-					$.maps.setCenter('map', this.lat, this.lon);
-					newPin = $.maps.placePin('map', this.lat, this.lon, '');
-				}else if (this.isKeyPoint){
-					newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTlarge');					
-				}else{
-					newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTsmall');					
-				}
-				newPins[this.id] = newPin;
-			});
-		});
+        updatePins();
+        
 	});
 	
 })(window.jQuery);
