f7593c1c025794573755ad24180b7fb518d15b9b
davetayls <dave@the-taylors.org>
2011-04-05 21:26:25 +0100

    added functionality to follow a complete set of locations

--- js/script.js
+++ js/script.js
@@ -6,50 +6,76 @@
 	var mapDetails,
 		newPins = {},
         counter = 0,
-        feedUrl = 'AndyExample.json',
-		manPin;
+        feedUrl = 'http://runfatboyrun.supportsasha.com/get.php?name=Andy3&date=2011-04-03&callback=?',
+		manPin,
+		$runningMan;
 	
     var sortByCreated = function(a,b){
         return a.created < b.created ? 1 : a.created > b.created ? -1 : 0;
     };
-    var updatePins = function(){
-		$.getJSON('AndyExample.' + counter + '.json', function(data, status){
+	var updateLocations = function(locations){
+        $(locations).each(function(i){
+            var newPin = newPins[this.id];
+            if (!newPin){
+                console.log(this.lat + ' ' + this.lon);
+                if (i === 0){
+					if (manPin){
+						manPin.moveTo(this.lat, this.lon);
+						$runningMan.attr('src', 'img/runseq.gif')
+						//$runningMan.attr('src', 'img/sprintseq.gif')
+					}else{
+                    	manPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTman');
+					}
+                    $.maps.setCenter('map', this.lat, this.lon);
+				}
+                if (this.isKeyPoint){
+                    newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTlarge');					
+                }else{
+                    newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTsmall');					
+                }
+                newPins[this.id] = newPin;
+            }
+        });		
+	};
+	var getLocations = function(url, callback){
+		$.getJSON(feedUrl, function(data, status){
             if (status === 'success'){
                 var locations = data.locations.sort(sortByCreated);
-                console.log({l:locations});
-                $(locations).each(function(i){
-                    var newPin = newPins[this.id];
-                    if (!newPin){
-                        console.log(this.lat + ' ' + this.lon);
-                        if (i === 0){
-							if (manPin){
-								manPin.moveTo(this.lat, this.lon);
-							}else{
-                            	manPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTman');
-							}
-                            $.maps.setCenter('map', this.lat, this.lon);
-						}
-                        if (this.isKeyPoint){
-                            newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTlarge');					
-                        }else{
-                            newPin = $.maps.placePin('map', this.lat, this.lon, 'cp-map-pinTsmall');					
-                        }
-                        newPins[this.id] = newPin;
-                    }
-                });
-                setTimeout(updatePins,3000);
+				callback.call(data, locations);
+            }
+		});		
+	};
+    var updatePins = function(){
+		$.getJSON(feedUrl, function(data, status){
+            if (status === 'success'){
+                var locations = data.locations.sort(sortByCreated);
+				updateLocations(locations);
+                //setTimeout(updatePins,3000);
             }
 		});
         counter ++;
     };
     
 	$(function(){
-		$('#map').maps({
-			centerPin: false
-		});
+		
+		$runningMan = $('#running-man-img');
+		
+		$('#map').maps({ centerPin: false });
 		
 		mapDetails = $.maps.getMapDetails('map');
-        updatePins();
+
+		getLocations(feedUrl, function(locations){
+			var i=0, run = locations, interval;
+			interval = setInterval(function(){
+				var location = run[i];
+				if (location){
+					updateLocations([location]);
+					i++;					
+				}else{
+					clearInterval(interval);
+				}
+			}, 1000)
+		});
         
 	});
 	
